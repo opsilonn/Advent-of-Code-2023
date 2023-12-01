708f9ebff18eb3c530c5634c444addebba5ea448
opsilonn <huguesbegeot@gmail.com>
2023-12-01 11:32:32 +0100

    Implémente l'exercice 01 - partie 2
    en prenant en compte les chiffres écrits sous forme de string

--- src/app/models/exercise-strategies/exercise-01-strategy.spec.ts
+++ src/app/models/exercise-strategies/exercise-01-strategy.spec.ts
@@ -29,11 +29,19 @@ describe('Exercise01Strategy', () => {
         expect(strategy.resolve(data)).toEqual(142);
     });
 
-    it('#resolve2 Devrait échouer', () => {
+    it('#resolve2 Quand on reçoit une liste de mots ayant des chiffres insérés, notamment sous forme de string, alors on calcule la somme des nombres composés des premiers et derniers chiffres de chaque ligne', () => {
         // Given
-        const data: string[] = [];
+        const data = [
+            'two1nine', // 29
+            'eightwothree', // 83
+            'abcone2threexyz', // 13
+            'xtwone3four', // 24
+            '4nineeightseven2', // 42
+            'zoneight234', // 14
+            '7pqrstsixteen', // 76
+        ];
 
         // When && Then
-        expect(() => strategy.resolve2(data)).toThrowError('Méthode non implémentée - 0');
+        expect(strategy.resolve2(data)).toEqual(281);
     });
 });
