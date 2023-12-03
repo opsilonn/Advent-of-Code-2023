f8d18dd135153bf691958c5a6049bbec6f7b6baa
opsilonn <huguesbegeot@gmail.com>
2023-12-03 23:55:12 +0100

    Implémente l'exercice 02 - partie 2

--- src/app/models/exercise-strategies/exercise-02-strategy.spec.ts
+++ src/app/models/exercise-strategies/exercise-02-strategy.spec.ts
@@ -30,11 +30,17 @@ describe('Exercise02Strategy', () => {
         expect(strategy.resolve(data)).toEqual(8);
     });
 
-    it('#resolve2 Devrait échouer', () => {
+    it('#resolve2 Quand on reçoit une liste de parties composées de différents tirages, alors retourne la somme des puissances minimales de toutes les parties', () => {
         // Given
-        const data: string[] = [];
+        const data: string[] = [
+            'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green', // 48
+            'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue', // 12
+            'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red', // 1560
+            'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red', // 630
+            'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green' // 36
+        ];
 
         // When && Then
-        expect(() => strategy.resolve2(data)).toThrowError('Méthode non implémentée - 0');
+        expect(strategy.resolve2(data)).toEqual(2286);
     });
 });
