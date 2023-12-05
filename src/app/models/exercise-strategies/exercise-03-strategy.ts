0fc2f1e9eba18e96b742fc96e966a4a383016546
opsilonn <huguesbegeot@gmail.com>
2023-12-05 19:38:47 +0100

    Implémente l'exercice 03, partie 2

--- src/app/models/exercise-strategies/exercise-03-strategy.ts
+++ src/app/models/exercise-strategies/exercise-03-strategy.ts
@@ -35,9 +35,24 @@ export class Exercise03Strategy implements ExerciseStrategy {
         return total;
     }
 
-    /** Méthode temporaire */
+    /**
+     * Pour une liste de string contenant des '*' cachées, retourne la somme des multiplications des nombres qui entourent chacun d'entre eux (il faut au moins 2 nombres autour)
+     * @param data Jeu de données à traiter
+     * @returns la somme des multiplications des nombres qui entourent chacun d'entre eux (il faut au moins 2 nombres autour)
+     */
     public resolve2(data: string[]): number {
-        throw Error('Méthode non implémentée - ' + data.length);
+        let total = 0;
+
+        for (let i = 0; i < data.length; i++) {
+            for (let j = 0; j < data[i].length; j++) {
+                const currentChar = data[i][j];
+                if (this.isStar(currentChar)) {
+                    total += this.getAdjacentTwoNumbers(data, i, j);
+                }
+            }
+        }
+
+        return total;
     }
 
     private isNumberSurrounded(data: string[], indexI: number, indexJ: number, currentNumber: string): boolean {
@@ -54,6 +69,38 @@ export class Exercise03Strategy implements ExerciseStrategy {
         return false;
     }
 
+    private getAdjacentTwoNumbers(data: string[], indexI: number, indexJ: number): number {
+        let numberArray: string[] = [];
+        let numberIndex = 0;
+
+        for (let i = indexI - 1; i <= indexI + 1; i++) {
+            if (this.isInRange(i, data)) {
+                for (let j = indexJ - 1; j <= indexJ + 1; j++) {
+                    if (this.isInRange(j, data[i])) {
+                        if (this.isNumber(data[i][j])) {
+                            numberArray[numberIndex] = '';
+                            while (this.isNumber(data[i][j-1])) {
+                                j--;
+                            }
+                            while (this.isNumber(data[i][j])) {
+                                numberArray[numberIndex] += data[i][j];
+                                j++;
+                            }
+                        }
+                        numberIndex++;
+                    }
+                }
+            }
+        }
+
+        numberArray = numberArray.filter((str: string) => !!str);
+        if (numberArray.length < 2) {
+            return 0;
+        }
+
+        return parseInt(numberArray[0], 10) * parseInt(numberArray[1], 10);
+    }
+
     private isNumber(char: string): boolean {
         return !isNaN(parseInt(char, this.BASE_10));
     }
@@ -64,6 +111,10 @@ export class Exercise03Strategy implements ExerciseStrategy {
 
     private isSpecialChar(char: string): boolean {
         return !this.isNumber(char) && !this.isDot(char);
+    }
+
+    private isStar(char: string): boolean {
+        return char === '*';
     }
 
     private isInRange(index: number, arr: string[] | string): boolean {
