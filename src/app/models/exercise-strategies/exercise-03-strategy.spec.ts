b42f0bc7d5972abc6ccb419600c8b771d9d8b57d
opsilonn <huguesbegeot@gmail.com>
2023-12-05 19:38:47 +0100

    Implémente l'exercice 03, partie 2

--- src/app/models/exercise-strategies/exercise-03-strategy.spec.ts
+++ src/app/models/exercise-strategies/exercise-03-strategy.spec.ts
@@ -35,11 +35,36 @@ describe('Exercise03Strategy', () => {
         expect(strategy.resolve(data)).toEqual(4361);
     });
 
-    it('#resolve2 Devrait échouer', () => {
+    it('#resolve2 Quand on reçoit une liste de string contenant des symboles \'*\', alors retourne la somme des multiplications des nombres qui l\'entourent', () => {
         // Given
-        const data: string[] = [];
+        const data = [
+            '467..114..',
+            '...*......',
+            '..35..633.',
+            '......#...',
+            '617*......',
+            '.....+.58.',
+            '..592.....',
+            '......755.',
+            '...$.*....',
+            '.664.598..',
+        ];
+        const total = 467*35 + 755*598;
 
         // When && Then
-        expect(() => strategy.resolve2(data)).toThrowError('Méthode non implémentée - 0');
+        expect(strategy.resolve2(data)).toEqual(total);
+    });
+
+    it('#resolve2 Quand on reçoit une liste de string contenant des symboles \'*\', alors retourne la somme des multiplications des nombres qui l\'entourent', () => {
+        // Given
+        const data = [
+            '............830..743.......59..955.......663..........................................367...........895....899...............826...220......',
+            '.......284.....*............*.....$...+.....*...377..................*.......419.............488...*.......*...................*..-....939..',
+            '....%.........976..679.461.7..........350..33.........$.380...$...151.897..........295..#......*....105.....418.............481........&....',
+        ];
+        const total = 830*976 + 59*7 + 663*33 + 151*897 + 895*105 + 899*418 + 826*481;
+
+        // When && Then
+        expect(strategy.resolve2(data)).toEqual(total);
     });
 });
