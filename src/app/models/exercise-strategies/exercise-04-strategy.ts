757e31a9d0471860e132afa17c5414fc1c885e6d
opsilonn <huguesbegeot@gmail.com>
2023-12-05 20:03:09 +0100

    Implémente l'exercice 04, partie 1

--- src/app/models/exercise-strategies/exercise-04-strategy.ts
+++ src/app/models/exercise-strategies/exercise-04-strategy.ts
@@ -2,13 +2,48 @@ import { ExerciseStrategy } from "src/app/models/exercise-strategy";
 
 export class Exercise04Strategy implements ExerciseStrategy {
 
-    /** Méthode temporaire */
+    private readonly BASE_10 = 10;
+
+    /**
+     * Pour une liste de chiffres vainqueurs / chiffres tirés, retourne la valeur de votre tirage
+     * @param data Jeu de données à traiter
+     * @returns la valeur de votre tirage
+     */
     public resolve(data: string[]): number {
-        throw Error('Méthode non implémentée - ' + data.length);
+        return data
+            .map((line: string) => this.readLine(line))
+            .reduce((sum: number, current: number) => sum + current, 0);
     }
 
     /** Méthode temporaire */
     public resolve2(data: string[]): number {
         throw Error('Méthode non implémentée - ' + data.length);
+    }
+
+    private readLine(line: string): number {
+        // On supprime le morceau "Partie XYZ :"
+        line = line.split(':')[1];
+
+        const [winningNumbersStr, elfNumbersStr]: string[] = line.split('|');
+        const winningNumbers: number[] = winningNumbersStr
+            .split(' ')
+            .filter((str: string) => !!str)
+            .map((str: string) => parseInt(str, this.BASE_10));
+        const elfNumbers: number[] = elfNumbersStr
+            .split(' ')
+            .filter((str: string) => !!str)
+            .map((str: string) => parseInt(str, this.BASE_10));
+
+        let total = 0;
+        elfNumbers.forEach((n: number) => {
+            if (winningNumbers.includes(n)) {
+                if (total === 0) {
+                    total = 1;
+                } else {
+                    total *= 2;
+                }
+            }
+        });
+        return total;
     }
 }
