4818f08e588874a7b093d7e0104aa579b318d9b1
opsilonn <huguesbegeot@gmail.com>
2023-12-06 23:05:19 +0100

    Implémente l'exercice 01 - partie 1

--- src/app/models/exercise-strategies/exercise-05-strategy.ts
+++ src/app/models/exercise-strategies/exercise-05-strategy.ts
@@ -1,14 +1,62 @@
 import { ExerciseStrategy } from "src/app/models/exercise-strategy";
 
+interface Map {
+    destination: number;
+    source: number;
+    range: number;
+}
+
 export class Exercise05Strategy implements ExerciseStrategy {
+
+    private readonly BASE_10 = 10;
 
     /** Méthode temporaire */
     public resolve(data: string[]): number {
-        throw Error('Méthode non implémentée - ' + data.length);
+        const seeds = data[0].split(': ')[1].split(' ').map((n: string) => parseInt(n, this.BASE_10));
+        const almanacs = this.getAlmanacs(data);
+        return Math.min(...this.convertSeedsToLocation(seeds, almanacs));
     }
 
     /** Méthode temporaire */
     public resolve2(data: string[]): number {
         throw Error('Méthode non implémentée - ' + data.length);
+    }
+
+    /** Méthode temporaire */
+    private getAlmanacs(data: string[]): Map[][] {
+        const almanacs: Map[][] = [];
+        let almanacsIndex = 0;
+        let index = 2;
+        
+        while(index < data.length) {
+            index++; // Pour ignorer la phrase "machin-to-machine map:"
+            almanacs[almanacsIndex] = [];
+
+            while(index < data.length && data[index] !== '') {
+                const [destination, source, range] = data[index].split(' ').map((str: string) => parseInt(str, this.BASE_10));
+                almanacs[almanacsIndex].push({ destination, range, source });
+                index++;
+            }
+
+            almanacsIndex++;
+            index++;
+        }
+
+        return almanacs;
+    }
+    
+    /** Méthode temporaire */
+    private convertSeedsToLocation(seeds: number[], almanacs: Map[][]): number[] {
+        const convertedSeeds = [...seeds];
+
+        for (let i = 0; i < seeds.length; i++) {
+            almanacs.forEach((almanac: Map[]) => {
+                almanac
+                    .filter((map: Map) => map.source <= convertedSeeds[i] && convertedSeeds[i] < map.source + map.range)
+                    .forEach((map: Map) => convertedSeeds[i] = convertedSeeds[i] - map.source + map.destination);
+            });
+        }
+
+        return convertedSeeds;
     }
 }
